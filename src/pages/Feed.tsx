656b6a01f08c0d46f0186b2095fa9adc40e6d377
kjy <rlawhdduq95@gmail.com>
2024-11-29 14:50:13 +0900

    페이지 작성(로그인, 게시글-구현중)

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,49 +1,104 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import { useUser } from "../components/UserContext";
+import apiClient from "../service/apiClient";
 
 const Feed: React.FC = () => {
-    const [data, setData] = useState<any>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<String | null>(null);
+    const[ board, setBoard ] = useState<any>(null);
+    const[ loading, setLoading ] = useState<boolean>(true);
+    const[ error, setError ] = useState<String | null>(null);
+    const{ user } = useUser();
+    const containerRef = useRef<HTMLDivElement>(null); // 스크롤 컨테이너 참조용
 
+    // useEffect는 1회성 함수이다.
+    // 처음 페이지가 호출될 때 실행되고 작업을 처리한 뒤 만약 상태를 저장하는 작업을 진행했을 경우
+    // 페이지가 렌더링되는것을 캐치한 뒤 다시 실행하는? 뭐 그런거라네
     useEffect(() => {
-        axios
-        .post(
-            "http://localhost:8988/api/v1/service",
+        if(user?.user_id && user?.token)
+        {
+            getBoard(user.user_id, user.token);
+            const container = containerRef.current;
+
+            if( container )
             {
-                "Body" : {
-                    user_id: 100,
-                    limit: 3,
-                    board_id: null,
+                container.addEventListener("scroll", handleScroll);
+            }
+
+            return () => {
+                if( container )
+                {
+                    container.removeEventListener("scroll", handleScroll);
                 }
-            },
+            }
+        }
+    }, [user, board]);
+    const getBoard = (user_id: string, token: string, board_id? : number | null) => {
+            apiClient
+                    .post(
+                        "/api/v1/service",
+                        {
+                            Body : {
+                                user_id: user_id,
+                                limit: 2,
+                                board_id: board_id,
+                            }
+                        },
+                        {
+                            headers: {
+                                "token": token,
+                                "call_url": "/api/board",
+                                "call_method": "GET",
+                            },
+                        })
+                        .then(response => {
+                            setBoard(response.data);
+                        })
+                        .catch(error => {
+                            console.error("Error : ", error);
+                        });
+            setLoading(false);
+    };
+
+    const handleScroll = () => {
+        if( containerRef.current )
+        {
+            const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+
+            if( scrollTop + clientHeight >= scrollHeight - 10 )
             {
-                headers: {
-                    "token": "",
-                    "call_url": "/api/board",
-                    "call_method": "GET",
-                },
-            })
-            .then(response => {
-                setData(response.data);
-                setLoading(false);
-            })
-            .catch(err => {
-                setError("Error fetching data");
-                setLoading(false);
-            });
-    }, []);
-
-    if(loading) return <p>Loading...</p>;
-    if(error) return <p>{error}</p>;
-
+                if(user?.user_id && user?.token && board !== null)
+                {
+                    const maxBoardNumber = Math.max(...board.map((item: { board_id: number; }) => item.board_id))
+                    console.log(maxBoardNumber);
+                    getBoard(user.user_id, user.token, maxBoardNumber);
+                }
+            }
+        }
+    }
+    // if(loading) return <p>Loading...</p>;
+    // if(error) return <p>{error}</p>;
     return (
-        <div>
+        <div ref={containerRef} style={{ height: "400px", overflowY: "auto", border: "1px solid black"}}>
             <h1>Welcome to the mydays Feed Page!</h1>
             <Link to="/todolist">Todolist</Link>
             <hr />
             <Link to="/viteintro">viteintro</Link>
+            <hr />
+            {board && Array.isArray(board) && (
+                <ul>
+                    {board.map((item: any, index: number) => (
+                        <li key={index}>
+                            <p>{item.board_id}</p>
+                            <p>{item.user_id}</p>
+                            <p>{item.scope_of_disclosure}</p>
+                            <p>{item.create_time}</p>
+                            <p>{item.fulfillment_time}</p>
+                            <p>{item.content}</p>
+                            <p>{item.update_time}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,10 +5,10 @@ const Profile: React.FC = () => {
     const{ user } = useUser();
     return (
         <div>
-            <h1>사용자 정보</h1>
+            <h1>사용자 정보 /</h1>
             {user 
             ? (
-                <p>안녕하세요, {user.id} 님! <br/> 회원유형 : {user.user_type}</p>
+                <p>안녕하세요, {user.id}님! <br/> 회원유형 : {user.user_type}</p>
             )
             : (
                 <p>로그인해주세요</p>
