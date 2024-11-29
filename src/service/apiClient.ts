92a484616b3e6fe7f238f65d0b042693299fde48
kjy <rlawhdduq95@gmail.com>
2024-11-29 14:50:22 +0900

    전역 axios 컴포넌트

---
+++ src/service/apiClient.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost',
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json", // 공통 요청 헤더타입 설정
+    }
+});
+// apiClient.interceptors.request.use((config) => {
+//     console.log("Request: ", config);
+//     return config;
+// });
+
+// apiClient.interceptors.response.use(
+//     (response) => {
+//         console.log("response= ");
+//         console.log(response.data);
+//         return response;
+//     },
+//     (error) => {
+//         console.error("Response Error: ", error);
+//         return Promise.reject(error);
+//     }
+// );
+export default apiClient;
