d5a19df59b67eacb38b7cec7d398ecc63b84851d
kjy <rlawhdduq95@gmail.com>
2024-12-03 17:29:47 +0900

    글쓰기 완성

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Profile from "./pages/Profile";
 import Todolist from "./test/Todolist";
 import ViteIntro from "./test/ViteIntro";
 import Login from "./pages/Login";
+import Write from "./pages/board/Write";
 // import "./App.css";
 
 const App: React.FC = () => {
@@ -26,6 +27,7 @@ const App: React.FC = () => {
         <Route path="/profile" element={<PrivateRoute component={Profile} />} />
         <Route path="/todolist" element={<PrivateRoute component={Todolist} />} />
         <Route path="/viteintro" element={<PrivateRoute component={ViteIntro} />} />
+        <Route path="/write" element={<PrivateRoute component={Write} />} />
 
         {/* 기본 페이지 */}
         <Route path="/" element={<PrivateRoute component={Feed} />} />
