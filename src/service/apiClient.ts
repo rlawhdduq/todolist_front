e11484ad9182c7101577bd6d435c86f097d8cc97
kjy <rlawhdduq95@gmail.com>
2024-12-16 20:38:44 +0900

    with credentials 추가가

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -6,22 +6,23 @@ const apiClient = axios.create({
     timeout: 5000,
     headers: {
         "Content-Type": "application/json", // 공통 요청 헤더타입 설정
-    }
+    },
+    withCredentials: true
 });
-// apiClient.interceptors.request.use((config) => {
-//     console.log("Request: ", config);
-//     return config;
-// });
+apiClient.interceptors.request.use((config) => {
+    console.log("Request: ", config);
+    return config;
+});
 
-// apiClient.interceptors.response.use(
-//     (response) => {
-//         console.log("response= ");
-//         console.log(response.data);
-//         return response;
-//     },
-//     (error) => {
-//         console.error("Response Error: ", error);
-//         return Promise.reject(error);
-//     }
-// );
+apiClient.interceptors.response.use(
+    (response) => {
+        console.log("response= ");
+        console.log(response.data);
+        return response;
+    },
+    (error) => {
+        console.error("Response Error: ", error);
+        return Promise.reject(error);
+    }
+);
 export default apiClient;
