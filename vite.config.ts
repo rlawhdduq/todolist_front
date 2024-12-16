ac9752e3fe94763fa11c4c9f8cf98de2874417c1
kjy <rlawhdduq95@gmail.com>
2024-12-16 23:17:26 +0900

    port 삭제제

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8989,
     watch: {
       usePolling: true, // 파일 변경 감지를 위해 폴링 사용
     },
